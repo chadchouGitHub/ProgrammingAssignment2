e999ea791b340a394de5d3f7963be0a3b03b1418
Chad Chou <chiachingchou@gmail.com>
2014-05-25 14:24:11 -0400

    Complete the assignment2
    Finalize with comments.

--- cachematrix.R
+++ cachematrix.R
@@ -9,32 +9,33 @@ makeCacheMatrix <- function(x = matrix()) {
 
         i <- NULL ## assign m is null, initinalize.
         set <- function(y) {
-                x <<- y
-                i <<- NULL
+                x <<- y  ##assign x with y outside the environment that was defined.
+                i <<- NULL ##assign i to null, and don't change outside this environment.
         }
-        get <- function() x
-        setinverse <- function(solve) i <<- solve
-        getinverse <- function() i
+        get <- function() x ## define get function by assign x vector to get.
+        setinverse <- function(solve) i <<- solve ## define setinverse function by assign inverse martrix result to i vector.
+        getinverse <- function() i ##define getinverse with i vector.
         list(set = set, get = get,
              setinverse = setinverse,
-             getinverse= getinverse)
+             getinverse= getinverse) ## return list of functions to makeCachMatrix function.
 
 
 }
 
 
-## Write a short comment describing this function
+## This function will find inverse matrix in catch before computing same matrix again.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 
-        i <- x$getinverse()
+        i <- x$getinverse() ##assign i vector by subseting x list's getinverse element.
+        ## Use if to check i to find the inverser in the memory.
         if(!is.null(i)) {
                 message("getting cached data")
-                return(i)
+                return(i) ## if i already in the data, just returen i to cachSolve function.
         }
-        data <- x$get()
-        i <- solve(data, ...)
-        x$setinverse(i)
-        i
+        data <- x$get() ##assign data vector with get function from subseting x list.
+        i <- solve(data, ...) ## caculate inverse matrix with solve function.
+        x$setinverse(i) 
+        i ## return i to cacheSolve function.
 }
